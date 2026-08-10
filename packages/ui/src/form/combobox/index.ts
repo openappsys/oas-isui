d2e33711e27d7786e08b82e9bf5db9bb8eb78349
zandy <znadyo@gmail.com>
2026-08-10 10:13:36 +0800

    feat(combobox): 新增可过滤单选组合框组件（输入框即控件、子串过滤、键盘导航、受控、clearable/loading/empty/disabled）+ PRD/ROADMAP v1.8 条目 + locale key + demo + 文档站侧栏注册

---
+++ packages/ui/src/form/combobox/index.ts
@@ -0,0 +1,8 @@
+import '@oas-isui/i18n'
+import { OASCombobox } from './oas-combobox.js'
+
+if (!customElements.get('oas-combobox')) {
+  customElements.define('oas-combobox', OASCombobox)
+}
+
+export { OASCombobox }
